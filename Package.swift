e41118a1dd77e422f17d950c98f6509cc8640c2c
Cristian Summa <160849789+SummaCristian@users.noreply.github.com>
2025-07-25 12:23:52 +0200

    refactor: Expand supported platforms and update tools version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,14 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "LocalizerKit",
     platforms: [
-        .iOS("16.0")
+        .iOS("16.0"),
+        .macOS("13.0"),
+        .tvOS("16.0"),
+        .visionOS("1.0")
     ],
     products: [
         .library(
